346446a152fa7747b7981f66ff8aa798146c312d
tara manicsic <tzmanics@gmail.com>
2013-10-31 20:39:00 -0400

    style changes to template & signup, logo image moved to webosphere

--- views/_v_template.php
+++ views/_v_template.php
@@ -4,21 +4,22 @@
 	<title><?php if(isset($title)) echo $title; ?></title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-	<link rel="stylesheet" type="text/css" href="css/master.css">
+	<link rel="stylesheet" type="text/css" href="/css/master.css">
 					
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 <header>
-	<div id="logo"><h2><a href="/index"><img src="images/caringCodersLogo.png"></a></h2></div>
+	<div id="logo"><h2><a href="/index"><img src="https://dl.dropboxusercontent.com/u/3061181/caringCodersLogo.png"></a></h2></div>
 	<nav>
 		<ul>
+			<li>Hi, <?php if($user) {echo $user->first_name;
+				} else {echo '<a href="/users/login">Log in?</a>';}?></li>
 			<li><a href="/posts/add">Ask</a></li>
 			<li><a href="/posts/posts">Read</a></li>
 			<li><a href="/users/profile">Profile</a></li>
-			<li>Hi, <?php if($user) {echo $user->first_name;
-				} else {echo '<a href="/users/login">Log in?</a>';}?></li>
+			
 		</ul>
 	</nav>
 </header>
--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,10 +1,11 @@
-<h2>SIGN UP</h2>
+
+<h2>Sign up & Join the Fun!</h2>
 <form method='POST' action='/users/p_signup'>
 
-	First Name <input type='text' name='first_name'><br>
-	Last Name <input type='text' name='last_name'><br>
-	Email <input type='text' name='email'><br>
-	Password <input type='password' name='password'><br>
+	First Name <input type='text' name='first_name'><br><br>
+	Last Name <input type='text' name='last_name'><br><br>
+	Email <input type='text' name='email'><br><br>
+	Password <input type='password' name='password'><br><br>
 
 	<input type='submit' value='Sign Up'>
 	<input type='hidden' name='timezone'>
